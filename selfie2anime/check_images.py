1c57a6c0bb5629bcdedde43580a031c4c5b91eb4
Aubrey Choi <lakshimi87@gmail.com>
2024-09-11 16:57:25 +0900

    selfie2anime: image check program add and modify

--- selfie2anime/check_images.py
+++ selfie2anime/check_images.py
@@ -1,18 +1,21 @@
 import cv2
 import os
 
+HistSize = 64
+
 PathFormat = "dataset/%s/"
 
 setName = input("set name: ")
 path = PathFormat%setName
 files = os.listdir(path)
 files = [f for f in files if f.endswith(".jpg") or f.endswith(".png")]
+files.sort()
 
 images = []
 for i in range(len(files)):
 	image = cv2.imread(path+files[i])
-	hist = cv2.calcHist([image], [0, 1, 2], None, [128, 128, 128], [0, 256, 0, 256, 0, 256])
-	hist[127, 127, 127] = 0 #ignore all white pixels
+	hist = cv2.calcHist([image], [0, 1, 2], None, [HistSize]*3, [0, 256, 0, 256, 0, 256])
+	#hist[HistSize-1, HistSize-1, HistSize-1] = 0 #ignore all white pixels
 	cv2.normalize(hist, hist, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
 	images.append(hist)
 
@@ -22,4 +25,4 @@ for i in range(len(files)-1):
 		image2 = images[j]
 		# Find the metric value
 		metric_val = cv2.compareHist(image1, image2, cv2.HISTCMP_CORREL)
-		if round(metric_val, 2) > 0.8: print(files[i], files[j], metric_val)
+		if round(metric_val, 2) > 0.95: print(files[i], files[j], metric_val)
