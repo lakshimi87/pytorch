4342a60fb70908449c53595bcd4293f162f9da24
Aubrey Choi <lakshimi87@gmail.com>
2024-11-07 11:14:08 +0900

    selfie2anime: modify bug fix.

--- selfie2anime/selfie2anime.py
+++ selfie2anime/selfie2anime.py
@@ -285,8 +285,8 @@ def sampleImages(batchesDone):
 timeStamp, epochSize = time.time(), len(dataLoader)*BatchSize
 epoch = InitEpoch
 # make valid and fake target
-valid = torch.ones((1, ImageHeight, ImageWidth, *DA.outputShape), device=device)
-fake = torch.zeros((1, ImageHeight, ImageWidth, *DA.outputShape), device=device)
+valid = torch.ones((1, ImgHeight, ImgWidth, *DA.outputShape), device=device)
+fake = torch.zeros((1, ImgHeight, ImgWidth, *DA.outputShape), device=device)
 while epoch < Epochs:
 	for i, batch in enumerate(dataLoader):
 		# get images from data set
